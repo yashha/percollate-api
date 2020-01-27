f91a35002b10a01e8c207ffa3013807ba94393ff
Y K <yasarkkk@protonmail.com>
2020-01-27 21:34:24 +0100

    added cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(helmet());
+  app.enableCors({
+    origin: 'https://readtheweb.de'
+  });
 
   await app.listen(process.env.PORT || 3000);
   console.log('Listening at http://localhost:3000');
