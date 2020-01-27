8ff0dcda313c022581365cea0a75da893fa9329e
Y K <yasarkkk@protonmail.com>
2020-01-27 21:27:09 +0100

    Organize imports

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import * as helmet from 'helmet';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
