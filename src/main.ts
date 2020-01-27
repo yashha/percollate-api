cfb98122316378022644d931315c76b7badd6324
Y K <yasarkkk@protonmail.com>
2020-01-27 21:26:33 +0100

    Added helmet

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+
   await app.listen(process.env.PORT || 3000);
   console.log('Listening at http://localhost:3000');
 }
