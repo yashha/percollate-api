0956b5381ac5ba25fa545354cf859b9db9937bdf
Y K <yasarkkk@protonmail.com>
2020-02-09 21:00:49 +0100

    changed docs path

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('thedocs')
   root(): string {
     return this.appService.root();
   }
